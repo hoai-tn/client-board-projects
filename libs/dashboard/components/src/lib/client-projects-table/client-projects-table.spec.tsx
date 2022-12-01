bcb23fc3458c3198a1dface68b4147919fbcddd0
hoai.tran <tranngochoai02051999@gmail.com>
2022-12-01 20:54:43 +0700

    [TEST] Write test case for client-projects-table.spec

--- libs/dashboard/components/src/lib/client-projects-table/client-projects-table.spec.tsx
+++ libs/dashboard/components/src/lib/client-projects-table/client-projects-table.spec.tsx
@@ -15,4 +15,38 @@ describe("Client Projects Table", () => {
     );
     expect(baseElement).toBeTruthy();
   });
+
+  it("should display fields table", () => {
+    const { baseElement } = render(
+      <ClientProjectTable
+        clientDataTable={clientOpenProjects}
+        tableHeadFields={clientOpenHead}
+      />
+    );
+
+    [
+      "Project Name",
+      "Start date",
+      "End date",
+      "Project Manager",
+      "Total Budget",
+      "$ Overhead Billed",
+      "Project Type",
+    ].forEach((item, index) => {
+      expect(
+        baseElement.querySelector(
+          `table>thead>tr>:nth-child(${index + 1}) span`
+        )?.textContent
+      ).toContain(item);
+    });
+  });
+
+  it("should fill Client Projects data on the table", () => {
+    const { baseElement } = render(
+      <ClientProjectTable
+        clientDataTable={clientOpenProjects}
+        tableHeadFields={clientOpenHead}
+      />
+    );
+  });
 });
